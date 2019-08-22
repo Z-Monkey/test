02edbf6b326ea61e11382958a85a75e5f80f75bd
Dr.Monkey <test@monkey.com>
2019-08-22 20:48:16 +0800

    分支dev完成User的set方法

--- src/main/java/com/test/domain/User.java
+++ src/main/java/com/test/domain/User.java
@@ -4,4 +4,15 @@ public class User {
 	private int id;
 	private String name;
 	private String address;
+	//分支dev完成User的set方法
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
 }
